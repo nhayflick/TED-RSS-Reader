15d09770594eb823d1efe64716feb99f8eca3063
Nate Hayflick <nhayflick@gmail.com>
2013-06-05 00:27:53 -0700

    adds inline comments

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+// Fetch RSS results
+
 var initialize = function() {
-  var that = this;
   var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
   feed.setNumEntries(100)
   feed.load(function(result){
     if(!result.error) {
-      console.log(result);
       $('.nav-title').html(result.feed.title);
-      window.onhashchange = function() {loadPage(result)};
-      loadPage(result);
+      window.onhashchange = function() {loadGrid(result)};
+      loadGrid(result);
     }
   });
 
 };
 
-var loadPage = function(result) {
+// Inserts 6 RSS tiles based on the current hash fragment pagination
+
+var loadGrid = function(result) {
   var entries = result.feed.entries;
   var contentEl = document.getElementById("content");
   var page = parseInt(window.location.hash.replace( /^\D+/g, '')) || 1;
@@ -24,6 +26,8 @@ var loadPage = function(result) {
     insertEntryDiv(entries[i], contentEl, i);
    }
 };
+
+// Loads a single RSS Feed entry
 
 var insertEntryDiv = function(entry, el, i){
   var containerDiv = document.createElement("div");
@@ -40,34 +44,31 @@ var insertEntryDiv = function(entry, el, i){
   containerDiv.addEventListener("click", function(e){showDetails(e, entry, containerDiv)}, false);
   el.appendChild(containerDiv);
   videojs("video-"+i, {}, function(){
-    // Player (this) is initialized and ready.
   });
   $("#show-"+i).on('hidden', function () {
     stopVideo("#video-"+i);
   })
 };
 
+// Start video - called when modal is opened
+
 var showDetails = function(event, entry, containerDiv){
-  console.log("clicked: "+entry.title)
-  console.log($(event.target).find("video"))
+  $(containerDiv).find("video")[0].load();
   $(containerDiv).find("video")[0].play();
 };
-
-google.load("feeds", "1");
-google.setOnLoadCallback(initialize);
+  
+// Stop video play - called when modal is shut
 
 var stopVideo = function(containerDiv){
   $(containerDiv).find("video")[0].pause();
 }
 
-var setPage = function(page){
-  window.location.hash = page
-  loadPage(result)
-}
+// Watch for url fragment changes and update contents accordingly
 
 var setupPagination = function(result){
-  window.onhashchange = loadPage(result)
+  window.onhashchange = loadGrid(result)
 }
+
 
 var updatePagination = function(page, entries){
   if(page > 1) {
@@ -82,3 +83,6 @@ var updatePagination = function(page, entries){
   }
   $("#page-num").html(page);
 }
+
+google.load("feeds", "1");
+google.setOnLoadCallback(initialize);
